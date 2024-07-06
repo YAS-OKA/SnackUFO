51aa8775d09acf16105c0da4d718c452c68a6515
YAS-OKA <108788135+YAS-OKA@users.noreply.github.com>
2024-07-06 17:13:18 +0900

    gitignore更新

--- SnackUFO/ProcedureManager/Condition.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "../stdafx.h"
-#include "Condition.h"
-
-using namespace prg;
-//
-//void MixedCondition::addChecker(Condition* checker, bool and_or)
-//{
-//	other << std::pair<bool, Condition*> {and_or, checker};
-//}
--- SnackUFO/ProcedureManager/Init.h
+++
@@ -1,5 +0,0 @@
-﻿#pragma once
-#include"Action.h"
-#include"Actions.h"
-#include"Condition.h"
-#include"GameAction.h"
